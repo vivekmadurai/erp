41498eb9e2126fc874b41970365b207e377f765c
Divyaadz <adzhomeindia@gmail.com>
2015-01-21 09:37:15 +0530

    Edit customer and employee

--- static/js/master.controller.js
+++ static/js/master.controller.js
@@ -7,11 +7,7 @@ app.config([ '$routeProvider', function($routeProvider) {
 	}).when('/Employees', {
 		templateUrl : 'static/views/employee.list.html',
 		controller : 'EmployeeCtr1'
-	}).when('/addCustomer', {
-		templateUrl : 'static/views/customer.form.html',
-		controller : 'CustomerCtrl'
 	})
-
 	.otherwise({
 		redirectTo : '/Customers'
 	});
@@ -49,10 +45,12 @@ app.controller("MasterCtrl", function($scope, $http) {
 	$scope.setSidebarSelectedTab = function(tabName) {
 		$scope.sidebarSelectedTab = tabName;
 	}
-
+	
+	
 });
 
-app.controller("EmployeeCtr1", function($scope, $http, $window, $location, $route) {
+app.controller("EmployeeCtr1", function($scope, $http, $window, $location,
+		$route) {
 	$scope.employees = [];
 	result = null;
 	$scope.currentPage = 0;
@@ -91,9 +89,9 @@ app.controller("EmployeeCtr1", function($scope, $http, $window, $location, $rout
 
 	};
 
-	$scope.createEmployee = function(User) {
+	$scope.createEmployee = function(User, employeeForm) {
 
-		if (!$scope.employeeForm.$valid) {
+		if (!employeeForm.$valid) {
 			alert("Please check the entered data.")
 			return;
 		}
@@ -111,121 +109,202 @@ app.controller("EmployeeCtr1", function($scope, $http, $window, $location, $rout
 			$('#addEmployeeModal').modal('hide');
 
 			// Loading Employee section alone.. No full page reload
-			$location = $window.location.origin + '/master#/Employees';			
+			$location = $window.location.origin + '/master#/Employees';
 			$route.reload();
-			
+
 		}).error(function(data, status, headers, config) {
 			$window.alert("Employee is not persisted")
 		});
 	}
+	
+	/*
+	 * Below functionality is used for save and save another.
+	 */
+	$scope.createEmployeeAndSaveAnother = function(User,
+			employeeForm) {
 
-});
+		$scope.successMsg = "";
 
-app.controller("CustomerCtrl", function($scope, $http, $window, $route) {
-
-	$scope.customers = [];
-	$scope.currentPageCustomers = [];
-	$scope.customer = null;
-	result = null;
-	$scope.currentPage = 0;
-	this.busy = false;
-
-	$scope.invokeLoadCustomers = function() {
-
-		$scope.loadCustomers(1000);
-	}
-
-	$scope.loadCustomers = function(limit) {
-
-		if (this.busy)
-			return;
-		this.busy = true;
-		$scope.selectedTab = 'Customers';
-
-		$scope.currentPage++;
-
-		$http.get("/Customer/list/p" + $scope.currentPage + "/" + limit)
-				.success(function(result) {
-
-					for (var index = 0; index < result.length; index++) {
-
-						$scope.customers.push(result[index]);
-					}
-
-				}).error(function() {
-					$scope.products = null;
-					alert("Unable to load Customer")
-				})
-
-		this.busy = false;
-
-	};
-
-	$scope.createCustomer = function(customer) {
-
-		if (!$scope.customerForm.$valid) {
+		if (!employeeForm.$valid) {
 			alert("Please check the entered data.")
 			return;
 		}
 
-		$http({
-			method : 'post',
-			url : '/Customer/create',
-			headers : {
-				'Content-Type' : 'application/x-www-form-urlencoded'
-			},
-			data : $.param(customer)
-		}).success(function(data, status, headers, config) {
+		$http(
+				{
+					method : 'post',
+					url : '/User/create',
+					headers : {
+						'Content-Type' : 'application/x-www-form-urlencoded'
+					},
+					data : $.param(User)
+				})
+				.success(
+						function(data, status, headers, config) {
 
-			$window.alert("Customer information is persisted successfully")
-			$('#addCustomerModal').modal('hide');
+							$scope.resetForm(employeeForm);
+							$scope.successMsg = "Employee is saved succesfully. Please save another employee"
 
-			// Loading Employee section alone.. No full page reload
-			$location = $window.location.origin + '/master#/Customers';			
-			$route.reload();
-			
+						})
+				.error(function(data, status, headers, config) {
+					$window.alert("Employee is not persisted")
+				});
 
-		}).error(function(data, status, headers, config) {
-			$window.alert("Customer is not persisted")
-		});
+	}
+	
+	$scope.resetForm = function(employeeForm) {
+		employeeForm.$setPristine();
+		$scope.User =  {};
+	}
+	
+	$scope.resetEmployeeAddForm = function(User) {
+	
+		$scope.User =  {};
+		
+	}
+	
+	$scope.openEmployeeModelWindow = function(employeeRow) {
+		$scope.User = employeeRow;
 	}
 
-	/*
-	 * Below functionality is used for save and save another. Success msg is
-	 * getting retained after between form submit in dom. This need to be
-	 * checked in later release.
-	 */
-
-	
-	  $scope.createCustomerAndSaveAnother = function(customer){ 
-		  
-		  $scope.successMsg = "";
-		  
-		  if (!$scope.customerForm.$valid) {
-				alert("Please check the entered data.")
-				return;
-			}
-
-		  
-		  $http({method :
-			     'post', url : '/Customer/create', headers : { 'Content-Type' :
-			     'application/x-www-form-urlencoded' }, data : $.param(customer)
-				  }).success(function(data, status, headers, config) {
-				  
-					  		$scope.resetForm();
-					  		$scope.successMsg = "Customer is saved succesfully. Please save another customer"
-				  
-				  }).error(function(data, status, headers, config) {
-					  $window.alert("Customer is not persisted") });
-					  
-	  }
-	 
-	  $scope.resetForm = function() { 
-		  //angular.copy({},$scope.customer);
-		  //$scope.customer.emailId = "";
-		  $scope.customerForm.$setPristine();
-		  //angular.copy({},$scope.customerForm); 
-	  }
-	 
-
 });
+
+app
+		.controller(
+				"CustomerCtrl",
+				function($scope, $http, $window, $route, $filter) {
+
+					$scope.customers = [];
+					$scope.currentPageCustomers = [];
+					// $scope.customer = null;
+
+					result = null;
+					$scope.currentPage = 0;
+					this.busy = false;
+
+					$scope.invokeLoadCustomers = function() {
+
+						$scope.loadCustomers(1000);
+					}
+
+					$scope.loadCustomers = function(limit) {
+
+						if (this.busy)
+							return;
+						this.busy = true;
+						$scope.selectedTab = 'Customers';
+
+						$scope.currentPage++;
+
+						$http
+								.get(
+										"/Customer/list/p" + $scope.currentPage
+												+ "/" + limit)
+								.success(
+										function(result) {
+
+											for (var index = 0; index < result.length; index++) {
+
+												$scope.customers
+														.push(result[index]);
+											}
+
+										}).error(function() {
+									$scope.products = null;
+									alert("Unable to load Customer")
+								})
+
+						this.busy = false;
+
+					};
+
+					$scope.createCustomer = function(customer, customerForm) {
+
+						if (!customerForm.$valid) {
+							alert("Please check the entered data.")
+							return;
+						}
+
+						$http(
+								{
+									method : 'post',
+									url : '/Customer/create',
+									headers : {
+										'Content-Type' : 'application/x-www-form-urlencoded'
+									},
+									data : $.param(customer)
+								})
+								.success(
+										function(data, status, headers, config) {
+
+											$window
+													.alert("Customer information is persisted successfully");
+											$('#addCustomerModal')
+													.modal('hide');
+
+											// Loading Employee section alone..
+											// No full page reload
+											$location = $window.location.origin
+													+ '/master#/Customers';
+											$route.reload();
+
+										})
+								.error(function(data, status, headers, config) {
+									$window.alert("Customer is not persisted")
+								});
+					}
+
+					/*
+					 * Below functionality is used for save and save another.
+					 */
+					$scope.createCustomerAndSaveAnother = function(customer,
+							customerForm) {
+
+						$scope.successMsg = "";
+
+						if (!customerForm.$valid) {
+							alert("Please check the entered data.")
+							return;
+						}
+
+						$http(
+								{
+									method : 'post',
+									url : '/Customer/create',
+									headers : {
+										'Content-Type' : 'application/x-www-form-urlencoded'
+									},
+									data : $.param(customer)
+								})
+								.success(
+										function(data, status, headers, config) {
+
+											$scope.resetForm(customerForm);
+											$scope.successMsg = "Customer is saved succesfully. Please save another customer"
+
+										})
+								.error(function(data, status, headers, config) {
+									$window.alert("Customer is not persisted")
+								});
+
+					}
+
+					$scope.resetForm = function(customerForm) {
+						customerForm.$setPristine();
+						$scope.customer =  {};
+					}
+					
+					$scope.resetCustomerAddForm = function(customer) {
+					
+						$scope.customer =  {};
+						
+					}
+
+					$scope.openwindow = function(customerRow) {
+
+						$scope.customer = customerRow;
+					}
+					
+					
+
+				});
