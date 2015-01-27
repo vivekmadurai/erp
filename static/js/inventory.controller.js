f319a8fd5f28e529c512020005b542119a1ef768
Divyaadz <adzhomeindia@gmail.com>
2015-01-27 08:37:24 +0530

    Modified Inventory to resemble as Master Tab

--- static/js/inventory.controller.js
+++ static/js/inventory.controller.js
@@ -1,129 +1,384 @@
-var app = angular.module("POSApp", ['ngRoute']);
+var app = angular.module("POSApp", [ 'ngRoute', 'infinite-scroll' ]);
 
-app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-      when('/Product List', {
-        templateUrl: 'static/views/product.list.html',
-        controller: 'ProductListCtrl'
-      }).
-      when('/Add Product', {
-        templateUrl: 'static/views/product.form.html',
-        controller: 'ProductCtrl'
-      }).
-      
-      when('/Category List', {
-        templateUrl: 'static/views/category.list.html',
-        controller: 'CategoryListCtrl'
-      }).
-      when('/Add Category', {
-        templateUrl: 'static/views/category.form.html',
-        controller: 'CategoryCtrl'
-      }).
-      otherwise({
-        redirectTo: '/Product List'
-      });
-}]);
+app.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/Products', {
+		templateUrl : 'static/views/category.list.html',
+		controller : 'ProductCtrl'
+
+	}).when('/Categories', {
+		templateUrl : 'static/views/category.list.html',
+		controller : 'CategoryCtrl'
+	}).otherwise({
+		redirectTo : '/Products'
+	});
+} ]);
 
 app.controller("InventoryCtrl", function($scope, $http) {
-    $scope.selectedTab = "Product List";
-    
-    $scope.setSelectedTab = function(tabName) {
-        $scope.selectedTab = tabName;
-    }
+
+	$scope.selectedTab = 'Products';
+
+	$scope.setSelectedTab = function(tabName) {
+		$scope.selectedTab = tabName;
+	}
+
 });
 
-app.controller("ProductListCtrl", function($scope, $http, $filter) {
+app
+		.controller(
+				"ProductCtrl",
+				function($scope, $http, $window, $route, $filter) {
+					$scope.products = [];
 
-    $scope.products = [];
-    $scope.product = null;
-    
-    //__init__ methods
-    loadProducts(1, 5);
-        
-    function loadProducts(offset, limit) {
-        $http.get("/Product/list/p"+offset+"/"+limit)
-        .success(function(result){
-            $scope.products = result;
-        }).error(function(){
-            alert("Unable to load products")
-        })
-    };
-    
-    $scope.openwindow = function(productId) {
-        $scope.product = $filter('filter')($scope.products, {instanceId: productId})     
-        document.getElementById('editProductWindow').show();  
-    }
-});
+					result = null;
+					$scope.currentPage = 0;
+					this.busy = false;
 
-app.controller("CategoryListCtrl", function($scope, $http, $filter) {
+					$scope.selectedTab = 'Products';
 
-    $scope.categories = [];
-    $scope.category = [];
-    //__init__ methods
-    loadCategories(1, 5);    
-    function loadCategories(offset, limit) {
-        $http.get("/Category/list/p"+offset+"/"+limit)
-        .success(function(result){
-            $scope.categories = result;
-        }).error(function(){
-            alert("Unable to load categories")
-        })
-    }
-    
-    $scope.openEditCategorywindow = function(categoryId) {
-        $scope.category = $filter('filter')($scope.categories, {instanceId: categoryId})     
-        document.getElementById('editCategoryWindow').show();  
-    }
-});
+					$scope.invokeLoadProducts = function() {
 
-app.controller("ProductCtrl", function($scope, $http) {
-    validateForm();
-    
-    $scope.categoryList = [];
-    
-    loadCategory();
-    
-    function loadCategory() {
-        $http.get("/Category/list").success(function(result){
-            $scope.categoryList = result;
-        }).error(function(){
-            alert("Unable to load category list")
-        })
-    }
-        
-    $scope.createProduct = function(product) {
-         $http({
-               method: 'post',
-               url:'/Product/create', 
-               headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-               data: $.param(product)
-            }).success(function(data, status, headers, config) {
-                // this callback will be called asynchronously
-                // when the response is available
-            }).
-              error(function(data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
-        }
-});
+						$scope.loadProducts(1000);
+					}
 
-app.controller("CategoryCtrl", function($scope, $http) {
-    validateForm();
-    
-    $scope.createCategory = function(category) {
-        $http({
-               method: 'post',
-               url:'/Category/create', 
-               headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-               data: $.param(category)
-          }).success(function(data, status, headers, config) {
-                // this callback will be called asynchronously
-                // when the response is available
-          }).
-          error(function(data, status, headers, config) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-          });
-    }
-});
+					$scope.loadProducts = function(limit) {
+
+						if (this.busy)
+							return;
+						this.busy = true;
+						$scope.selectedTab = 'Products';
+
+						$scope.currentPage++;
+
+						$http
+								.get(
+										"/Product/list/p" + $scope.currentPage
+												+ "/" + limit)
+								.success(
+										function(result) {
+
+											for (var index = 0; index < result.length; index++) {
+
+												$scope.products
+														.push(result[index]);
+											}
+
+										}).error(function() {
+									$scope.products = null;
+									alert("Unable to load Product")
+								})
+
+						this.busy = false;
+
+					};
+
+					$scope.createProduct = function(Product, productForm,
+							productInstanceId) {
+
+						if (!productForm.$valid) {
+							alert("Please check the entered data.")
+							return;
+						}
+
+						if (productInstanceId == null) {
+							$scope.invokeAddProduct(Product, productForm);
+						} else if (productInstanceId != null) {
+							$scope.invokeUpdateProduct(Product, productForm,
+									productInstanceId);
+						}
+
+					}
+
+					$scope.invokeAddProduct = function(Product, productForm) {
+
+						$http(
+								{
+									method : 'post',
+									url : '/Product/create',
+									headers : {
+										'Content-Type' : 'application/x-www-form-urlencoded'
+									},
+									data : $.param(Product)
+								})
+								.success(
+										function(data, status, headers, config) {
+
+											$window
+													.alert("Product information is persisted successfully");
+											$('#addProductModal').modal('hide');
+
+											// Loading Employee section alone..
+											// No full page reload
+											$location = $window.location.origin
+													+ '/master#/Products';
+											$route.reload();
+
+										})
+								.error(function(data, status, headers, config) {
+									$window.alert("Product is not persisted")
+								});
+					}
+
+					$scope.invokeUpdateProduct = function(Product, productForm,
+							productInstanceId) {
+
+						$http(
+								{
+									method : 'put',
+									url : '/Product/' + productInstanceId
+											+ '/update',
+									headers : {
+										'Content-Type' : 'application/x-www-form-urlencoded'
+									},
+									data : $.param(Product)
+								})
+								.success(
+										function(data, status, headers, config) {
+
+											$window
+													.alert("Product information is persisted successfully");
+											$('#addProductModal').modal('hide');
+
+											// Loading Employee section alone..
+											// No full page reload
+											$location = $window.location.origin
+													+ '/master#/Products';
+											$route.reload();
+
+										})
+								.error(function(data, status, headers, config) {
+									$window.alert("Product is not persisted")
+								});
+					}
+
+					/*
+					 * Below functionality is used for save and save another.
+					 */
+					$scope.createProductAndSaveAnother = function(Product,
+							productForm) {
+
+						$scope.successMsg = "";
+
+						if (!productForm.$valid) {
+							alert("Please check the entered data.")
+							return;
+						}
+
+						$http(
+								{
+									method : 'post',
+									url : '/Product/create',
+									headers : {
+										'Content-Type' : 'application/x-www-form-urlencoded'
+									},
+									data : $.param(Product)
+								})
+								.success(
+										function(data, status, headers, config) {
+
+											$scope.resetForm(productForm);
+											$scope.successMsg = "Product is saved succesfully. Please save another Product"
+
+										})
+								.error(function(data, status, headers, config) {
+									$window.alert("Product is not persisted")
+								});
+
+					}
+
+					$scope.resetForm = function(productForm) {
+						productForm.$setPristine();
+						$scope.Product = {};
+					}
+
+					$scope.resetProductAddForm = function(product) {
+
+						$scope.product = {};
+
+					}
+
+					$scope.openwindow = function(product) {
+
+						$scope.product = product;
+					}
+
+				});
+
+app
+.controller(
+		"CategoryCtrl",
+		function($scope, $http, $window, $route, $filter) {
+			$scope.categories = [];
+
+			result = null;
+			$scope.currentPage = 0;
+			this.busy = false;
+
+			$scope.selectedTab = 'Categories';
+
+			$scope.invokeLoadCategories = function() {
+
+				$scope.loadCategories(1000);
+			}
+
+			$scope.loadCategories = function(limit) {
+
+				if (this.busy)
+					return;
+				this.busy = true;
+				$scope.selectedTab = 'Categories';
+
+				$scope.currentPage++;
+
+				$http
+						.get(
+								"/Category/list/p" + $scope.currentPage
+										+ "/" + limit)
+						.success(
+								function(result) {
+
+									for (var index = 0; index < result.length; index++) {
+
+										$scope.categories
+												.push(result[index]);
+									}
+
+								}).error(function() {
+							$scope.categories = null;
+							alert("Unable to load Category")
+						})
+
+				this.busy = false;
+
+			};
+
+			$scope.createCategory = function(Category, categoryForm,
+					categoryInstanceId) {
+
+				if (!categoryForm.$valid) {
+					alert("Please check the entered data.")
+					return;
+				}
+
+				if (categoryInstanceId == null) {
+					$scope.invokeAddCategory(Category, categoryForm);
+				} else if (categoryInstanceId != null) {
+					$scope.invokeUpdateCategory(Category, categoryForm,
+							categoryInstanceId);
+				}
+
+			}
+
+			$scope.invokeAddCategory = function(Category, categoryForm) {
+
+				$http(
+						{
+							method : 'post',
+							url : '/Category/create',
+							headers : {
+								'Content-Type' : 'application/x-www-form-urlencoded'
+							},
+							data : $.param(Category)
+						})
+						.success(
+								function(data, status, headers, config) {
+
+									$window
+											.alert("Category information is persisted successfully");
+									$('#addCategoryModal').modal('hide');
+
+									// Loading Employee section alone..
+									// No full page reload
+									$location = $window.location.origin
+											+ '/master#/Categories';
+									$route.reload();
+
+								})
+						.error(function(data, status, headers, config) {
+							$window.alert("Category is not persisted")
+						});
+			}
+
+			$scope.invokeUpdateCategory = function(Category, categoryForm,
+					categoryInstanceId) {
+
+				$http(
+						{
+							method : 'put',
+							url : '/Category/' + categoryInstanceId
+									+ '/update',
+							headers : {
+								'Content-Type' : 'application/x-www-form-urlencoded'
+							},
+							data : $.param(Category)
+						})
+						.success(
+								function(data, status, headers, config) {
+
+									$window
+											.alert("Category information is persisted successfully");
+									$('#addCategoryModal').modal('hide');
+
+									// Loading Employee section alone..
+									// No full page reload
+									$location = $window.location.origin
+											+ '/master#/Categories';
+									$route.reload();
+
+								})
+						.error(function(data, status, headers, config) {
+							$window.alert("Category is not persisted")
+						});
+			}
+
+			/*
+			 * Below functionality is used for save and save another.
+			 */
+			$scope.createCategoryAndSaveAnother = function(Category,
+					categoryForm) {
+
+				$scope.successMsg = "";
+
+				if (!categoryForm.$valid) {
+					alert("Please check the entered data.")
+					return;
+				}
+
+				$http(
+						{
+							method : 'post',
+							url : '/Category/create',
+							headers : {
+								'Content-Type' : 'application/x-www-form-urlencoded'
+							},
+							data : $.param(Category)
+						})
+						.success(
+								function(data, status, headers, config) {
+
+									$scope.resetForm(categoryForm);
+									$scope.successMsg = "Category is saved succesfully. Please save another Category"
+
+								})
+						.error(function(data, status, headers, config) {
+							$window.alert("Category is not persisted")
+						});
+
+			}
+
+			$scope.resetForm = function(categoryForm) {
+				categoryForm.$setPristine();
+				$scope.Category = {};
+			}
+
+			$scope.resetCategoryAddForm = function(category) {
+
+				$scope.category = {};
+
+			}
+
+			$scope.openwindow = function(category) {
+
+				$scope.category = category;
+			}
+
+		});
